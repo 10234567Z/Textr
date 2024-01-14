6fa79fcd5cdcfb724e5a0650c4e19ca66b3211e4
10234567Z <93607971+10234567Z@users.noreply.github.com>
2024-01-14 10:10:41 +0530

    Send POST request after submitting new message

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,12 @@ router.get('/new', function (req, res, next) {
 });
 
 router.post('/new', function (req, res) {
-  
+  messages.push({
+    text: req.body.text,
+    user: req.body.user,
+    date: new Date()
+  })
+  res.send("Successfully sent the message")
 })
 
 module.exports = router;
