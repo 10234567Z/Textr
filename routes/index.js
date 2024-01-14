4266e8d9ca7a5176ee0eb1368abfa0b81b7afab5
10234567Z <93607971+10234567Z@users.noreply.github.com>
2024-01-14 08:14:25 +0530

    Add initial messages for future testing purposes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    message: "Hello There , I am first",
+    user: "Danny",
+    date: new Date(),
+  },
+  {
+    message: "That makes me second",
+    user: "Amanda",
+    date: new Date(),
+  },
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Textr' });
